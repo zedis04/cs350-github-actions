e9536dff2d801485b4d2fad86b99e91bd92b8c45
zedis04 <63256323+zedis04@users.noreply.github.com>
2023-04-05 14:11:25 +0900

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,11 @@
+def triangle(a, b, c):
+    if a < 0 or b < 0 or c < 0:
+        return -1
+    elif a + b < c or b + c < a or c + a < b:
+        return -1
+    elif a == b or b == c or c == a:
+        return 1
+    elif a == b and b == c:
+        return 2
+    else:
+        return 4
